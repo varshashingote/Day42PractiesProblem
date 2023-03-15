b2cd0dd79712db2ee60fc24eb1f18e44f1960384
varsha Gunjal <varshashingote01@gmail.com>
2023-03-15 15:22:43 +0530

    [varsha] Restrict Special char at Last

---
+++ RestrictCharAtLast.js
@@ -0,0 +1,18 @@
+console.log(" Welcome To PinCode Validation ");
+
+const pincodeRegex = RegExp("^[0-9]{6}$");
+
+function validatePincode(pincode){
+    if(pincodeRegex.test(pincode))
+        console.log(pincode + " : Valid Pin Code !!");
+    else
+        throw pincode + " : PINCODE is Invalid !";
+}
+
+try{
+    validatePincode("400088");
+    validatePincode("400088B");
+}
+catch(e){
+    console.error(e);
+}
